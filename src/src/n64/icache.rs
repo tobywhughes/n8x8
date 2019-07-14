62e809c61051e331a037101833d4d65181b9a7c9
Toby Hughes <tobywhughes@gmail.com>
2019-07-13 23:42:24 -0700

    Translates virtual addresses for icache into cache lines index

--- src/src/n64/icache.rs
+++ src/src/n64/icache.rs
@@ -1,15 +1,29 @@
-//16kb
-//512 lines
+pub struct ICache { 
+    pub line: Vec<ICacheLine>
+}
 
-pub struct CacheLine {
+impl ICache { 
+    pub fn new() -> ICache { 
+        return ICache {
+            line: vec![ICacheLine::new(); 512]
+        }
+    }
+
+    pub fn parse_line_index_from_virtual_index(&self, virtual_address: u32) -> u32{
+        return (virtual_address & 0x00003FE0) >> 5;
+    }
+}
+
+#[derive(Clone)]
+pub struct ICacheLine {
     pub valid: bool,
     physical_tag: u32,
     pub data: Vec<u32>,
 }
 
-impl CacheLine { 
-    pub fn new() -> CacheLine {
-        return CacheLine {
+impl ICacheLine { 
+    pub fn new() -> ICacheLine {
+        return ICacheLine {
             valid: false,
             physical_tag: 0,
             data: vec![0_u32;8]
--- src/src/n64/icache_tests.rs
+++ src/src/n64/icache_tests.rs
@@ -4,7 +4,14 @@ mod icache_tests
     use n64::icache::*;
 
     #[test]
-    fn name() {
+    fn can_translate_virtual_address_to_cache_line() {
+        let mut icache: ICache = ICache::new();
 
+        let line_index_high: u32 = icache.parse_line_index_from_virtual_index(0x00003FE0);
+        let line_index_low: u32 = icache.parse_line_index_from_virtual_index(0xFFFFC01F);
+
+
+        assert_eq!(511, line_index_high);
+        assert_eq!(0, line_index_low);
     }
 }
